541232b3cc78cc0a1b417912aaebb275f4d9bac0
Seif Mansour <162506632+MansourSeif@users.noreply.github.com>
2024-09-01 12:26:51 +0200

    first commit

---
+++ frontend/notes-app/src/components/Inputs/PasswordInput.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useState } from 'react'
+import { FaRegEye , FaRegEyeSlash} from 'react-icons/fa'
+
+const PasswordInput = ({value , onChange , placeholder}) => {
+    const [showPassword, setShowPassword] = useState(false)
+
+    const toggleShowPassword = () => {
+        setShowPassword(!showPassword)
+    }
+ 
+    return ( 
+    <div className='flex items-center bg-transparent border-[1.5px] px-5 rounded mb-3 '>
+        <input value={value} onChange={onChange} placeholder={placeholder || 'Password'} className='w-full bg-transparent text-sm py-3 mr-3 rounded outline-none '
+            type={showPassword ? 'text' : 'password'}  />
+
+        { showPassword?
+        <FaRegEye size={22} className='text-gray-500 cursor-pointer' onClick={() =>toggleShowPassword()} />
+        : 
+        <FaRegEyeSlash size={22} className='text-gray-500 cursor-pointer' onClick={() =>toggleShowPassword()} />
+        }
+
+        </div>
+  )
+}
+
+export default PasswordInput
